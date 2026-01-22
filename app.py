43fbc37761bbb9aa394f9e683d59a214579f34e8
Viktor226 <vanistratenko.a.5@gmail.com>
2026-01-22 09:40:27 +0200

    Add success survey form in app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+st.title("Анкета за успех")
+
+
+text = st.text("Въведи име и оценка")
+st.text_input ()
+st.number_input()
+subject = st.selectbox("Избери предмет") 
+["Математика", "Англиски език", "програмиране", "биология" ]
+text = st.text("Вуведи какво спортуваш")
+st.text_input()
+
+st.button ()
+
